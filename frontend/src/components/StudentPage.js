eadc32f5064a8dbe16ffd2534199a13df9dec375
Shaikh Abdul Rehman <atabdilrehman786@gmail.com>
2025-03-09 02:26:38 +0530

    add student page

--- frontend/src/components/StudentPage.js
+++ frontend/src/components/StudentPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function StudentPage(){
     return(
         <div>
-            <h2>Student page</h2>
-            <p>Welcome Student</p>
+        <div className='container-teacher'>
+        <h2 className='text-focus-in'>Welcome Student!</h2>
+        <div className='inputs-landing'>
+        <Link to="/register/teacher"><button className='button-teacher-register'>Register</button></Link>
+        <Link to="/login/teacher"><button className='button-teacher-login'>Login</button></Link>
         </div>
+        </div>
+    </div>
     );
 }
 
