d0aff085cce3bb6928978b73e78bfb5d2db5b9b0
Shaikh Abdul Rehman <atabdilrehman786@gmail.com>
2025-03-08 15:52:18 +0530

    add teacher register page

--- frontend/src/components/TeacherRegister.js
+++ frontend/src/components/TeacherRegister.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import email_icon from '../Assets/mail.png'
+import password_icon from '../Assets/pass.png'
+import user_icon from '../Assets/user_3.png'
+import './style/teacher-register.css'
+
 
 function TeacherRegister() {
     const [name, setName] = useState('');
@@ -17,12 +22,46 @@ function TeacherRegister() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button type="submit">Register</button>
-        </form>
+        // <form onSubmit={handleSubmit}>
+        //     <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+        //     <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        //     <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        //     <button type="submit">Register</button>
+        // </form>
+
+<div className='container'>
+<div className="header">
+<img src={user_icon} alt="" />
+<div className="text">SignUp</div>
+{/* <div className="underline"></div> */}
+</div>
+<form onSubmit={handleSubmit}>
+<div className='inputs'>
+
+    <div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text" placeholder='Name'value={name} onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <div className='line'></div>
+
+    <div className='input'>
+    <img src={email_icon} alt="" />
+<input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+</div>
+<div className='line'></div>
+<div className='input'>
+<img src={password_icon} alt="" />
+<input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+</div>
+<div className='line'></div>
+
+<div className='submit-container'>
+<button className='submit-btn' type="submit">Login</button>
+</div>
+</div>
+</form>
+
+</div>
     );
 }
 
