24d248acd77ccc462b5f51273092c5c20d6a466f
Shaikh Abdul Rehman <atabdilrehman786@gmail.com>
2025-03-19 21:26:36 +0530

    add navbar to teacher home page

--- frontend/src/components/TeacherHome.js
+++ frontend/src/components/TeacherHome.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import './style/landing.css';
+import './style/teacherHome.css'
+import { Menu } from 'lucide-react'; //hamburger 
+import logo_icon from '../Assets/Sensei-logo.png'
+import profile from '../Assets/profile_icon.png'
+
+
 function TeacherHome() {
-    return <div>
-        <h4>This Page displays the insights of the logged in Teacher, such as profile views, numbers of students tried contacting,etc</h4>
-    </div>; // Empty page for now
+    return <header>
+    {/* Hamburger Menu */}
+    <div className="hamburger">
+      <Menu size={35} />
+    </div>
+
+    {/* Logo Section */}
+    <div className="logo-container"><img src={logo_icon} alt="logo" className='logo'></img>
+         {/* <div className='logo-text'>SENSEI</div> */}
+    </div>
+
+    {/* Profile Section */}
+    <div className="profile">
+      <img src={profile} alt="Profile" />
+    </div>
+  </header>
 }
 
 export default TeacherHome;
