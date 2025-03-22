d39e36cee2a3aab7eebced83c2a1d4ee655cd07f
Shaikh Abdul Rehman <atabdilrehman786@gmail.com>
2025-03-22 15:43:29 +0530

    add profile photo and add profile dropdown

--- frontend/src/components/TeacherHome.js
+++ frontend/src/components/TeacherHome.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import './style/teacherHome.css'
+import React, { useState } from 'react';
+import './style/teacherHome.css';
 import { Menu } from 'lucide-react'; //hamburger 
-import logo_icon from '../Assets/Sensei-logo.png'
-import profile from '../Assets/profile_icon.png'
+import logo_icon from '../Assets/Sensei-logo.png';
+import profile from '../Assets/profile_icon.png';
 
 
 function TeacherHome() {
-    return <header>
-    {/* Hamburger Menu */}
-    <div className="hamburger">
-      <Menu size={35} />
-    </div>
+  //profile photo drop down
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    {/* Logo Section */}
-    <div className="logo-container"><img src={logo_icon} alt="logo" className='logo'></img>
-         {/* <div className='logo-text'>SENSEI</div> */}
-    </div>
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
 
-    {/* Profile Section */}
-    <div className="profile">
-      <img src={profile} alt="Profile" />
-    </div>
-  </header>
+  const handleOptionClick = (option) => {
+    console.log(`${option} clicked`);
+    setIsDropdownOpen(false); // Close dropdown after selection
+  };
+
+  return (
+    <header>
+      {/* Hamburger Menu */}
+      <div className="hamburger">
+        <Menu size={35} />
+      </div>
+
+      {/* Logo Section */}
+      <div className="logo-container">
+        <img src={logo_icon} alt="logo" className='logo' />
+        <div className='logo-text'>SENSEI</div>
+      </div>
+
+      {/* Profile Section */}
+      <div className="profile-container">
+        <div className="profile" onClick={toggleDropdown}>
+          <img src={profile} alt="Profile" />
+        </div>
+        
+        {isDropdownOpen && (
+          <div className="dropdown">
+            <ul>
+              <li onClick={() => handleOptionClick('View Profile')}>View Profile</li>
+              <li onClick={() => handleOptionClick('Edit Profile')}>Edit Profile</li>
+              <li onClick={() => handleOptionClick('Logout')}>Logout</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 }
 
 export default TeacherHome;
